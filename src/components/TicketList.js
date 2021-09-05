84b81ba1e11a7188cd7025df48796c73f7d9d8fa
Karlson Drendel <kdrendel99@gmail.com>
2021-09-05 12:07:29 -0700

    Initial commit

---
+++ src/components/TicketList.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Ticket from "./Ticket";
+import PropTypes from "prop-types";
+
+
+// const masterTicketList = [
+//   {
+//     names: 'Thato and Haley',
+//     location: '3A',
+//     issue: 'Firebase won\'t save record. Halp.'
+//   },
+//   {
+//     names: 'Sleater and Kinney',
+//     location: '4B',
+//     issue: 'Prop types are throwing an error.'
+//   },
+//   {
+//     names: 'Imani & Jacob',
+//     location: '9F',
+//     issue: 'Child component isn\'t rendering.'
+//   }
+// ];
+
+function TicketList(props){
+  return (
+    <React.Fragment>
+      <hr />
+      {props.ticketList.map((ticket) =>
+      <Ticket 
+      whenTicketClicked = {props.onTicketSelection}
+      names={ticket.names}
+      location={ticket.location}
+      issue={ticket.issue}
+      id={ticket.id}
+      key={ticket.id}/>
+      )}
+    </React.Fragment>
+
+
+    // <React.Fragment>
+    //   <hr/>
+    //   {masterTicketList.map((ticket, index) =>
+    //   <Ticket names={ticket.names}
+    //   location={ticket.location}
+    //   issue={ticket.issue}
+    //   key={index}/>
+    //   )}
+    // </React.Fragment>
+  );
+}
+
+// Add propTypes for ticketList.
+TicketList.propTypes = {
+  ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func
+};
+
+export default TicketList;
